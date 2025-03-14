17979b307833c96ddb06c3ddd8844213b99e8be4
Your Name <you@example.com>
2025-03-14 03:02:51 +0000

    refactor: simplify webpack stats configuration in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,31 +1,25 @@
 
-      /** @type {import("next").NextConfig} */
-      const config = {
-        images: {
-          remotePatterns: [
-            {
-              protocol: 'https',
-              hostname: '*',
-              pathname: '**',
-            },
-          ],
-        },
-        eslint: {
-          ignoreDuringBuilds: true,
-        },
-        typescript: {
-          ignoreBuildErrors: true,
-        },
-        webpack: (config, { isServer }) => {
-          config.stats = {
-            preset: 'verbose',
-            bodySizeLimit: undefined,
-            allowedOrigins: undefined
-          };
-          return config;
-        },
-        experimental: {
-          serverActions: true
-        }
-      };
-      export default config;
+/** @type {import("next").NextConfig} */
+const config = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '*',
+        pathname: '**',
+      },
+    ],
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  webpack: (config) => {
+    config.stats = 'normal';
+    return config;
+  }
+};
+
+export default config;
